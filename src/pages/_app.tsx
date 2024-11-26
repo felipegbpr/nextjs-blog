1f4c9dc35f64e989271161b5b056f88526715f26
Felipe Borges <felipetecacc@gmail.com>
2024-11-26 14:12:46 -0300

    Fixing AppProps

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import { AppProps } from 'next/dist/next-server/lib/router/router';
+import { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
 import { GlobalStyles } from '../styles/global-styles';
-import styled, { ThemeProvider } from 'styled-components';
 import { theme } from '../styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
