094a3e166cf433fcbaf446635b16b90a82549e51
Felipe Borges <felipetecacc@gmail.com>
2024-11-28 14:49:43 -0300

    Add url production

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -2,4 +2,4 @@ export const API_URL = 'https://blog-strapi-api-cnwn.onrender.com';
 export const POSTS_URL = `${API_URL}/posts`;
 
 export const SITE_NAME = 'Felipe Gomes';
-export const SITE_URL = process.env.SITE_LOCAL || 'http://127.0.0.1:3000';
+export const SITE_URL = 'https://bespoke-parfait-71ce4c.netlify.app';
