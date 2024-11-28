ff4fc3a521ccb1b4dffaf6c990112d2953c97332
Felipe Borges <felipetecacc@gmail.com>
2024-11-28 13:03:35 -0300

    Fix: Add deploy:static script

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,9 +1,9 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
+import { Post } from '../../containers/Post';
 import { countAllPosts } from '../../data/posts/count-all-posts';
 import { getAllPosts } from '../../data/posts/get-all-posts';
 import { getPost } from '../../data/posts/get-post';
 import { PostData } from '../../domain/posts/post';
-import { Post } from '../../containers/Post';
 
 export type DynamicPostProps = {
   post: PostData;
@@ -14,9 +14,11 @@ const DynamicPost = ({ post }: DynamicPostProps) => {
 };
 
 export default DynamicPost;
+
 export const getStaticPaths: GetStaticPaths = async () => {
   const numberOfPosts = await countAllPosts();
   const posts = await getAllPosts(`_limit=${numberOfPosts}`);
+
   return {
     paths: posts.map((post) => {
       return {
@@ -28,8 +30,10 @@ export const getStaticPaths: GetStaticPaths = async () => {
     fallback: false,
   };
 };
+
 export const getStaticProps: GetStaticProps = async (ctx) => {
   const posts = await getPost(ctx.params.slug);
+
   return {
     props: { post: posts[0] },
     // revalidate: 5,
