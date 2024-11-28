fbfe8d1814942cb85bb3608508bde026484ef829
Felipe Borges <felipetecacc@gmail.com>
2024-11-28 15:39:52 -0300

    Adding <Head /> to containers

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -1,5 +1,5 @@
 export const API_URL = 'https://blog-strapi-api-cnwn.onrender.com';
 export const POSTS_URL = `${API_URL}/posts`;
 
-export const SITE_NAME = 'Felipe Gomes';
+export const SITE_NAME = 'Blogntx';
 export const SITE_URL = 'https://bespoke-parfait-71ce4c.netlify.app';
