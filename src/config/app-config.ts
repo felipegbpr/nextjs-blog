f88cceb594ee80c930992ce6e3ac05fd856792dc
Felipe Borges <felipetecacc@gmail.com>
2024-11-28 22:13:17 -0300

    Changing website domain

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -2,4 +2,4 @@ export const API_URL = 'https://blog-strapi-api-cnwn.onrender.com';
 export const POSTS_URL = `${API_URL}/posts`;
 
 export const SITE_NAME = 'Blogntx';
-export const SITE_URL = 'https://bespoke-parfait-71ce4c.netlify.app';
+export const SITE_URL = 'https://blogntx.netlify.app';
